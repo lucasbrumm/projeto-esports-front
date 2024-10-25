bbebade67a747dcd481b039f02e63c49b0528765
lucas.lugao <lucasbrumfurtado@hotmail.com>
2024-10-24 22:31:48 -0300

    build: :bug: downsgrade web-vitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import { Metric } from 'web-vitals'
+
+type ReportHandler = (metric: Metric) => void
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals').then((webVitals) => {
+      webVitals.getCLS(onPerfEntry)
+      webVitals.getFID(onPerfEntry)
+      webVitals.getFCP(onPerfEntry)
+      webVitals.getLCP(onPerfEntry)
+      webVitals.getTTFB(onPerfEntry)
+    })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
